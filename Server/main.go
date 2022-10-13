aedf931b6ebdf01b498f94767fc152518703eee4
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-13 08:25:58 +0700

    update

--- Server/main.go
+++ Server/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"shirt/Server/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root@tcp(127.0.0.1:3306)/shirtgo?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	r.GET("/products", func(c *gin.Context) {
+		var products []models.Product
+		db.Find(&products)
+		c.JSON(200, products)
 	})
 	r.Run()
 }
---
+++ Server/models/products.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Id     uint
+	Gender string
+	Style  string
+	Size   string
+	Price  uint
+}
