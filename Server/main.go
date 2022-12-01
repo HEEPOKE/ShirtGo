862a2567d1b17e3494092ce61f6f87b922133f79
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-01 21:40:33 +0700

    update main

--- Server/main.go
+++ Server/main.go
@@ -1,70 +1,20 @@
-package main
+package amin
 
 import (
-	"net/http"
-	"shirt/Server/models"
+	"fmt"
+	"shirt/Server/database"
+	"shirt/Server/routes"
 
-
-"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/jho/godotenv"
 )
 
-unc main() {
-	dsn := "roottcp(127.0.0.1:3306)/shirtgo?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func main() {
+	err := godotenvLoad(".env")
 	if err != nil {
-	if err != nil {
-		panic("failed to connect database")
+		mt.Println("Error loading .env file")
 	}
 
-	r := gin.Default()
-	r.GET("/products", func(c *gin.Context) {
-		var products []models.Product
-		db.Find(&products)
-		c.JSON(200, products)
-	})
+	database.Connect()
+	routes.Router()
 
-	r.GET("/products/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		var products models.Product
-		db.First(&products, id)
-		c.JSON(200, products)
-	})
-
-	r.POST("/products", func(c *gin.Context) {
-		var product models.Product
-		if err := c.ShouldBindJSON(&product); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		result := db.Create(&product)
-		c.JSON(200, gin.H{"RowsAffected": result.RowsAffected})
-	})
-
-	r.PUT("/products/:id", func(c *gin.Context) {
-		var product models.Product
-		var updateProduct models.Product
-		if err := c.ShouldBindJSON(&product); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		db.First(&updateProduct, product.ID)
-		updateProduct.Gender = product.Gender
-		updateProduct.Style = product.Style
-		updateProduct.Size = product.Size
-		updateProduct.Price = product.Price
-		c.JSON(200, updateProdct)
-	})
-
-r.DELETE("/products/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		var products modelsProduct
-		db.First(&products, id)
-		db.Delete(&products)
-		c.JSON(200, products
-	})
-
-// r.Use(cors.Default())
-	r.Run()
 }
--- Server/routes/route.go
+++ Server/routes/route.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-contrib/cors"
 )
 
-func mapUrls() {
+func Router() {
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST"},
